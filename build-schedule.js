36ed1f8323800f923f6b6db905234246ae578b62
quicksurfcoop-cloud <quicksurfcoop@googlemail.com>
2026-09-15 21:48:01 +0100

    Update build-schedule.js

--- build-schedule.js
+++ build-schedule.js
@@ -32,36 +32,7 @@ const CHANNELS = [
   {
     id: 'krissy-one',
     name: 'Krissy ONE',
-    includeTitles: [
-      'Annika', 
-      'Blue Lights', 
-      'Bones', 
-      'Boston Blue', 
-      'Brilliant Minds', 
-      'Buffy the Vampire Slayer', 
-      'The Chelsea Detective', 
-      'Chicago Med', 
-      'Chicago P.D.', 
-      'Cold Case', 
-      'Code Black', 
-      'Criminal Minds', 
-      'CSI: Crime Scene Investigation', 
-      'Dexter', 
-      'The Doctor Blake Mysteries', 
-      'ER', 
-      'Firefly', 
-      'Found', 
-      'Happy Valley', 
-      'House', 
-      'Luther', 
-      'The Mentalist', 
-      'Midsomer Murders', 
-      'Monk', 
-      'Rizzoli & Isles', 
-      'Shetland', 
-      'Southland', 
-      'Watson'
-    ]
+    includeTitles: ['Annika']
   },
   { 
     id: 'BBC', 
