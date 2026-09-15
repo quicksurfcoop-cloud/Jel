a5aa01bb3e3830ecfa005c93145b22b7403c16d3
quicksurfcoop-cloud <quicksurfcoop@googlemail.com>
2026-09-15 21:13:34 +0100

    Update build-schedule.js

--- build-schedule.js
+++ build-schedule.js
@@ -32,8 +32,8 @@ const CHANNELS = [
    {
     id: 'Krissy One',
     name: 'Krissy ONE',
-    includeTitles: ['Annika', 'Blue Lights', 'Bones', 'Boston Blue', 'Brilliant Minds', 'Buffy the Vampire Slayer', 'The Chelsea Detective', 'Chicago Med', 'Chicago P.D.', 'Cold Case', 'Code Black', 'Criminal Minds', 'CSI: Crime Scene Investigation', 'Dexter', 'The Doctor Blake Mysteries', 'ER', 'Firefly', 'Found
-', 'Happy Valley', 'House', 'Luther', 'The Mentalist', 'Midsomer Murders', 'Monk', 'Rizzoli & Isles', 'Shetland', 'Southland', 'Watson'], // Replace these with your actual Jellyfin show titles
+    includeTitles: ['Annika', 'Blue Lights', 'Bones', 'Boston Blue', 'Brilliant Minds', 'Buffy the Vampire Slayer', 'The Chelsea Detective', 'Chicago Med', 'Chicago P.D.', 'Cold Case', 'Code Black', 'Criminal Minds', 'CSI: Crime Scene Investigation', 'Dexter', 'The Doctor Blake Mysteries', 'ER', 'Firefly',
+                    'Found', 'Happy Valley', 'House', 'Luther', 'The Mentalist', 'Midsomer Murders', 'Monk', 'Rizzoli & Isles', 'Shetland', 'Southland', 'Watson'], // Replace these with your actual Jellyfin show titles
     includeItemTypes: ['Series']
   },
   { 
